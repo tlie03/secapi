006bbb15cb8aec8a5b122a1bd27c1c6bc917f4e6
uwipq <uwipq@kit.edu>
2022-06-18 09:43:41 +0200

    .

--- secapi/filing_query/filing_query.py
+++ secapi/filing_query/filing_query.py
@@ -1,14 +1,14 @@
-from secapi.util.keymapper.key_mapper import KeyMapper
+from secapi.util.sec.key_mapper import KeyMapper
+from secapi.util.sec.constants import FORM_TYPES, FILING_INFORMATION_KEYS
 from secapi.util.date.date_range import DateRange
 from secapi.util.request.request_limitation import limited_request
 from secapi.util.filetypes.file_types import JSON_FILE
+from warnings import warn
 
 
 BASE_URL_SUBMISSIONS = r'https://data.sec.gov/submissions/'
 BASE_URL_ARCHIVE = r'https://www.sec.gov/Archives/edgar/data/'
 HEADER = {'User-Agent': 'myUserAgent'}
-
-FILING_TYPES = ['3', '4', '5']  # todo add filing types
 
 CIK_STRING = r'CIK'
 REQUIRED_CIK_LENGTH = 10
@@ -19,26 +19,29 @@ class FilingQuery:
     def __init__(self):
         self._key_mapper = KeyMapper()
 
-    def get_filings(self, ticker_symbol, filing_types=None, date_from=None, date_to=None):
-        if filing_types is None:
-            filing_types = FILING_TYPES
-        cik = self._key_mapper.get_cik(ticker_symbol.upper())
-        date_range = DateRange(date_from, date_to)
-        submissions_dict = self._get_submissions(cik)
+    def get_filings(self, ticker_symbol, date_from=None, date_to=None, form_types=None, filing_information=None):
+        cik = self._key_mapper.get_cik(ticker_symbol)
+        daterange = DateRange(date_from=date_from, date_to=date_to)
 
-        return self._parse_submissions_dict(submissions_dict, date_range, filing_types)
+        if form_types is None:
+            form_types = FORM_TYPES
+        else:
+            for form_type in form_types:
+                if form_type not in FORM_TYPES:
+                    warn("form_types list contains form types that either do not exist or are not supported")
+                    form_types.remove(form_type)
+
+        if filing_information is None:
+            filing_information = FILING_INFORMATION_KEYS
+        else:
+            # this way the ordering of the keys is always the same
+            cache_list = []
+            for key in FILING_INFORMATION_KEYS:
+                if key in filing_information:
+                    cache_list.append(filing_information.remove(key))
+
+            if len(filing_information) > 0:
+                warn("filing_information list contained keys that do not exist")
+            filing_information = cache_list
 
 
-    def _get_submissions(self, cik):
-        submissions_url = self._build_submissions_url(cik)
-        response = limited_request(url=submissions_url, header=HEADER)
-        return response.json()
-
-    @staticmethod
-    def _build_submissions_url(cik):
-        length_diff = REQUIRED_CIK_LENGTH - len(cik)
-        cik = ('0' * length_diff) + cik
-        return BASE_URL_SUBMISSIONS + '/' + CIK_STRING + cik + JSON_FILE
-
-    def _parse_submissions_dict(self, submissions_dict, daterange, filings_types):
-        pass
---
+++ secapi/util/sec/constants.py
@@ -0,0 +1,19 @@
+
+FORM_TYPES = ['3',
+                '4',
+                '5']  # todo add filing types
+
+FILING_INFORMATION_KEYS = ['accessionNumber',
+                           'filingDate',
+                           'reportDate',
+                           'acceptanceDateTime',
+                           'act',
+                           'form',
+                           'fileNumber',
+                           'filmNumber',
+                           'items',
+                           'size',
+                           'isXBRL',
+                           'isInlineXBRL',
+                           'primaryDocument',
+                           'primaryDocDescription']
