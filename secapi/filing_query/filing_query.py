3fbee4cb9fd391aaccb92c0ce514cfb0369a1e30
uwipq <uwipq@kit.edu>
2022-06-19 12:16:25 +0200

    .

--- secapi/filing_query/filing_query.py
+++ secapi/filing_query/filing_query.py
@@ -34,14 +34,34 @@ class FilingQuery:
         if filing_information is None:
             filing_information = FILING_INFORMATION_KEYS
         else:
-            # this way the ordering of the keys is always the same
+            # this way the ordering of the keys is always the same which is important for the parsing process
             cache_list = []
             for key in FILING_INFORMATION_KEYS:
                 if key in filing_information:
                     cache_list.append(filing_information.remove(key))
-
             if len(filing_information) > 0:
                 warn("filing_information list contained keys that do not exist")
             filing_information = cache_list
 
+        length_diff = REQUIRED_CIK_LENGTH - len(cik)
+        cik = ('0' * length_diff) + cik
+        submissions_url = BASE_URL_SUBMISSIONS + '/' + CIK_STRING + cik + JSON_FILE
 
+        response = limited_request(url=submissions_url, header=HEADER)
+        submissions_dict = response.json
+
+        return self._parse_submissions(submissions_dict, daterange, form_types, filing_information)
+
+
+    @staticmethod
+    def _parse_submissions(submissions_dict, daterange, form_types, filing_information):
+        # parse recent
+        recent = submissions_dict['recent']
+        recent_filing_count = len(recent['accessionNumber'])
+        recent_from = recent['filingDate'][recent_filing_count - 1]
+        recent_to = recent['filingDate'][0]
+
+        recent_date_range = DateRange(date_from=recent_from, date_to=recent_to)
+        if daterange.intersect(recent_date_range):
+            for i in range(recent_filing_count):
+                pass
