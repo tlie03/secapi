8b38db70e995714183246d6d354bf883895accc6
tlie03 <acc@tl1806.de>
2023-07-02 22:18:12 +0200

    changed a testcase and did some refactoring

--- src/secapi_tl/request.py
+++ src/secapi_tl/request.py
@@ -16,19 +16,18 @@ SESSION = requests.Session()
 FAKER = Faker()
 
 
-def sec_request(url: str):
-    """
-    I dont provide the sec_request_decorated function because for some reason the ratelimit decorator does not
-    work when some function calls to the decorated function are done locally in this python package and others are
-    done from outside the package. I dont know why this is the case but it is. For example using sec_request_decorated
-    and get_filings in the same project can cause problems. So I provide this function instead which is just a wrapper
-    """
-    return sec_request_decorated(url)
-
-
 @sleep_and_retry
 @limits(calls=SEC_REQUEST_COUNT, period=SEC_PERIOD)
-def sec_request_decorated(url: str):
+def sec_request(url: str):
+    """
+    This method performs the request to the sec.gov domain.
+    It is rate limited to SEC_REQUEST_COUNT requests per SEC_PERIOD seconds.
+    Thereby it ensures that the amount of requests stays in the boundaries set by the sec.
+
+    :param url: the url to request
+    :return: the response (a response object from the requests module)
+    :raises ConnectionError: if the response status code is not 200
+    """
     header = {"User-Agent": create_user_agent()}
     try:
         response = SESSION.get(url=url, headers=header)
@@ -36,10 +35,16 @@ def sec_request_decorated(url: str):
         raise err
 
     if response.status_code != 200:
+        print(response.text)
         raise ConnectionError(f"invalid response status code {response.status_code}")
     return response
 
 
 def create_user_agent() -> str:
+    """
+    creates a random user agent string that will be accepted by the sec.
+    A valid user agent string is required for all requests to the sec.gov domain.
+    It contains a first name, last name and email address.
+    """
     ua = f"{FAKER.first_name()} {FAKER.last_name()} {FAKER.email()}"
     return ua
--- testing/request_tests/different_module_test/module2/requester2.py
+++ testing/request_tests/different_module_test/module2/requester2.py
@@ -1,15 +1,26 @@
 from typing import List
 from threading import Thread
 from src.secapi_tl import get_registered_ciks, get_filings
+from ratelimit import limits, sleep_and_retry
 
+
+CALLS = 1
+PERIOD = 2
 THREAD_COUNT = 5
 ciks_total = get_registered_ciks()
+
+
+@sleep_and_retry
+@limits(calls=CALLS, period=PERIOD)
+def limited_get_filings(ticker_symbol: str) -> List[str]:
+    filings = get_filings(ticker_symbol=ticker_symbol)
+    return filings
 
 
 def thread_func(ciks: List[str]):
     for i, cik in enumerate(ciks):
         print(f"{i} / {len(ciks)}")
-        filings = get_filings(ticker_symbol=cik)
+        filings = limited_get_filings(cik)
         print(len(filings))
 
 
