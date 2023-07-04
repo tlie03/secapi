573ed44a1460edd1f43b9f2d021f37b81026381d
tlie03 <acc@tl1806.de>
2023-07-04 14:18:46 +0200

    changed version and added TooManyRequestsError to init file

--- src/secapi_tl/__init__.py
+++ src/secapi_tl/__init__.py
@@ -1,16 +1,19 @@
 from .filing_query import get_filings
-from .request import sec_request
+from .request import sec_request, TooManyRequestsError
 from .key_mapper import (is_registered, ticker_to_cik, filter_tickers, get_registered_tickers, get_registered_ciks)
 from .filing import Filing
 
 filing_query = ['get_filings']
 
-util = ['sec_request',
-        'is_registered',
-        'get_cik',
-        'filter_tickers',
-        'get_registered_tickers',
-        'get_registered_ciks']
+util = [
+    'sec_request',
+    'is_registered',
+    'get_cik',
+    'filter_tickers',
+    'get_registered_tickers',
+    'get_registered_ciks',
+    'TooManyRequestsError'
+]
 
 filing = ['Filing']
 
